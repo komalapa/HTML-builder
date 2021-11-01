8f27d93fa4e1d9f04eeb8b2626141c2ceb9b9685
komalapa <komalapa@gmail.com>
2021-11-01 21:21:16 +0400

    feat: 02 write file from stdin

---
+++ 02-write-file/index.js
@@ -0,0 +1,42 @@
+//Импорт всех требуемых модулей.
+const fs = require('fs');
+const path = require('path');
+
+const testFileName = 'text.txt';
+const testFilePath =path.join(__dirname, testFileName);
+
+
+// Создание потока записи в текстовый файл
+
+function writeOutput (outputFile){
+  if (!outputFile) {
+    throw new Error ('no file name specified');
+  } 
+  let stream = fs.createWriteStream(outputFile,  {flags: 'a'}); //append mode
+  return stream;
+}
+
+// Вывод в консоль приветственного сообщения
+console.log(`All your input will be saved in file: ${testFilePath} \n Enter 'exit' to stop`);
+
+// Ожидание ввода текста пользователем, с дальнейшей проверкой ввода на наличие ключевого слова exit
+
+function getInput (){
+  let stream = process.stdin;
+  return stream;    
+}
+
+// Запись текста в файл
+// Ожидание дальнейшего ввода
+getInput().pipe(writeOutput(testFilePath));
+
+// Реализация прощального сообщения при остановке процесса
+process.on('exit', function (){
+  console.log(`You stopped writing to the file ${testFilePath}. \n Goodbye!`);
+});
+//ctrl+c event => process.exit()
+process.on('SIGINT', process.exit);
+//catch "exit" on stdin
+process.stdin.addListener('data', function(data) {
+  if (data.toString().trim().toLowerCase() == 'exit') process.exit();
+});
